e839a7168f7676e94efcb8a2367485ff9df4889e
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-09-30 18:35:40 +0200

    Models

---
+++ prog6212-ice-4-ST10249838/Models/TaskModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ICE4.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [MaxLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+        public string? Title { get; set; }
+
+        [MaxLength(500, ErrorMessage = "Description cannot exceed 500 characters")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Deadline is required")]
+        [DataType(DataType.Date, ErrorMessage = "Invalid date format")]
+        public DateTime Deadline { get; set; }
+    }
+}
